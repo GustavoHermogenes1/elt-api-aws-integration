f7855b3b04b7d88f6767ae4abfe1f1fbfaff80e9
Gustavo <gustavo.hermogenes.03@gmail.com>
2026-02-19 19:43:27 -0300

    Criação da conversão json to parquet

--- src/processing/json_to_parquet.py
+++ src/processing/json_to_parquet.py
@@ -1,1 +1,22 @@
-...
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import io
+
+def transform_json_to_parquet(data:list[dict], output_path:str) -> io.BytesIO:
+    try:
+        df = pd.json_normalize(data)
+
+        table = pa.Table.from_pandas(df)
+        
+        buffer = io.BytesIO()
+        pq.write_table(table, buffer)
+
+        buffer.seek(0)
+
+        print(f'Arquivo Parquet salvo em:\n{output_path}')
+        
+        return buffer
+    
+    except Exception as e:
+        print(f'Erro ao converter os dados .json para parquet:\n{e}')
