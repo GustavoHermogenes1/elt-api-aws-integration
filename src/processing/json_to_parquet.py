b4161a592c93816d06b02dd1a778ebaa89f21279
Gustavo <gustavo.hermogenes.03@gmail.com>
2026-03-12 17:41:53 -0300

    Commit Final

--- src/processing/json_to_parquet.py
+++ src/processing/json_to_parquet.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-<<<<<<< HEAD
 import os
-
 
 def transform_json_to_parquet(data: dict, file_name: str, dir_path: str):
 
@@ -40,28 +38,3 @@ def transform_json_to_parquet(data: dict, file_name: str, dir_path: str):
 
     except Exception as e:
         raise Exception(f"Erro na transformação JSON -> Parquet: {e}")
-=======
-import io
-import os
-
-def transform_json_to_parquet(data:list[dict], file_name:str, dir_path:str) -> io.BytesIO:
-    try:
-        df = pd.json_normalize(data)
-
-        table = pa.Table.from_pandas(df)
-        
-        buffer = io.BytesIO()
-        pq.write_table(table, buffer)
-
-        buffer.seek(0)
-
-        if dir_path:
-            output_path = os.path.join(dir_path, file_name)
-            pq.write_table(table, output_path)
-            print(f'Arquivo Parquet salvo em:\n{output_path}')
-        
-        return buffer
-    
-    except Exception as e:
-        print(f'Erro ao converter os dados .json para parquet:\n{e}')
->>>>>>> 7b1d72ea7b5f713b66d843d3764b88cf2dbb4da2
--- src/storage/upload_s3.py
+++ src/storage/upload_s3.py
@@ -1,7 +1,6 @@
 import boto3
 import io
 import json
-<<<<<<< HEAD
 from pathlib import Path
 
 def upload_data_to_s3(data, bucket_s3: str, file_path: str):
@@ -26,20 +25,3 @@ def upload_data_to_s3(data, bucket_s3: str, file_path: str):
     s3.upload_fileobj(buffer, bucket_s3, file_path)
 
     print(f"Upload realizado: s3://{bucket_s3}/{file_path}")
-=======
-
-def upload_data_to_s3(data: dict | list | io.BytesIO, bucket_s3:str, file_path:str) -> None:
-    try:
-        s3 = boto3.client('s3')
-
-        if isinstance(data, io.BytesIO):
-            buffer = data
-        else:
-            buffer = io.BytesIO(json.dumps(data).encode("utf-8"))
-
-        s3.upload_fileobj(buffer, bucket_s3, file_path)
-        print(f'Upload de arquivo feito no bucket:\n{bucket_s3}')
-
-    except Exception as e:
-        print(f'Erro ao enviar arquivo para o S3: {e}')
->>>>>>> 7b1d72ea7b5f713b66d843d3764b88cf2dbb4da2
